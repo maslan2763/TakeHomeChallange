dfd1553f1d2e6a0486553254852050caf9822947
maslan63 <duslerotesi6223@gmail.com>
2022-10-16 13:07:25 -0400

    TranslationPage is adjusted

--- src/test/java/com/task/pages/TranslationPage.java
+++ src/test/java/com/task/pages/TranslationPage.java
@@ -1,11 +1,42 @@
 package com.task.pages;
 
+import com.task.utilities.Driver;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class TranslationPage extends BasePage{
+
+    public TranslationPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//*[@id='itamenu']/span/div/a[1]")
+    public WebElement screenKeyboard;
+
+    @FindBy(xpath = "//*[@id='K16']")
+    public WebElement upperCase;
+
+    @FindBy(xpath = "//*[@id='K72']")
+    public WebElement letterH;
+
+    @FindBy(xpath = "//*[@id='K73']")
+    public WebElement letterI;
+
+    @FindBy(xpath = "//*[@id='K49']")
+    public WebElement exclamationMark;
+    public void typeHi(){
+
+        screenKeyboard.click();
+        upperCase.click();
+        letterH.click();
+        letterI.click();
+        upperCase.click();
+        exclamationMark.click();
+    }
 
 }
