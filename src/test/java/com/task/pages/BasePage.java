5eed6a515401d758b1c369b6d0fbcba525e7885a
maslan63 <duslerotesi6223@gmail.com>
2022-10-16 13:07:05 -0400

    BasePage is adjusted

--- src/test/java/com/task/pages/BasePage.java
+++ src/test/java/com/task/pages/BasePage.java
@@ -17,48 +17,32 @@ public abstract class BasePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy (xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[1]/c-wiz/div[2]/button/div[3]")
+    @FindBy (xpath = "(//div[@class='VfPpkd-Bz112c-RLmnJb'])[1]")
     public WebElement sourceLanguageArrow;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[2]/c-wiz/div[1]/div/div[2]/input")
+    @FindBy(xpath = "(//input[@class='yFQBKb'])[1]")
     public WebElement sourceLanguageInput;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[2]/c-wiz/div[1]/div/div[4]/div/div[1]")
+    @FindBy(xpath = "(//div[@class='vSUSRc'])[2]")
     public WebElement SourceLanguage;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[1]/c-wiz/div[5]/button/div[3]")
+    @FindBy(xpath = "(//div[@class='VfPpkd-Bz112c-RLmnJb'])[3]")
     public WebElement translationLanguageArrow;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[2]/c-wiz/div[2]/div/div[2]/input")
+    @FindBy(xpath = "(//input[@class='yFQBKb'])[2]")
     public WebElement translationLanguageInput;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[2]/c-wiz/div[2]/div/div[4]/div/div[1]")
+    @FindBy(xpath = "(//div[@class='vSUSRc'])[4]")
     public WebElement translationLanguage;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[1]/span/span/div/textarea") //
+    @FindBy(xpath = "//textarea[@aria-label='Source text']") //
     public WebElement initialTextInput;
 
-    @FindBy(xpath = "(//button[@class='VfPpkd-Bz112c-LgbsSe VfPpkd-Bz112c-LgbsSe-OWXEXe-e5LLRc-SxQuSe yHy1rc eT1oJ mN1ivc qiN4Vb lRTpdf U2dVxe'])[1]")
+    @FindBy(xpath = "(//div[@class='VfPpkd-Bz112c-RLmnJb'])[2]")
     public WebElement swappingBtn;
 
-    @FindBy(xpath = "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div/div[8]/div/div[1]/span[1]/span/span")
+    @FindBy(xpath = "//span[@class='ryNqvb']")
     public WebElement resultGoogle;
-
-    @FindBy (xpath = "//a[@class='ita-kd-icon-button ita-kd-inputtool-icon ita-kd-small ita-kd-left']")
-    public WebElement screenKeyboard;
-
-    @FindBy(xpath = "//*[@id='K16']")
-    public WebElement upperCase;
-
-    @FindBy(xpath = "//*[@id='K72']")
-    public WebElement letterH;
-
-    @FindBy(xpath = "//*[@id='K73']")
-    public WebElement letterI;
-
-    @FindBy(xpath = "//*[@id='K49']")
-    public WebElement exclamationMark;
-
 
 
 
